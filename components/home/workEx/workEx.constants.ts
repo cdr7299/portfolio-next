c773893df3f9e9bf4b75daf7f939858ed3c04f25
Vineet Sawhney <gadgetgeek95@gmail.com>
2023-09-06 06:02:26 -0400

    Added education projects

--- components/home/workEx/workEx.constants.ts
+++ components/home/workEx/workEx.constants.ts
@@ -11,7 +11,7 @@ const work_data: WorkData[] = [
       {
         project_title: "Gan's Landing Pages",
         description:
-          "# Gan Studio's landing pages. \n ● Utilized Framer Motion for intricate animations on homepage\n \n ● Mobile And Tab Friendly Layouts",
+          "# Gan Studio's landing pages. \n ## ☉ Utilized Framer Motion for intricate animations on homepage \n ## ☉ Mobile And Tab Friendly Layouts",
         tech_used: [
           { title: "", icon: "/assets/tech/next3.png" },
           { title: "", icon: "/assets/tech/tailwind.png" },
@@ -23,8 +23,7 @@ const work_data: WorkData[] = [
       {
         project_title: "Myna",
         description:
-          "# Myna - Gan Studio's SaaS platform. \n Myna is Gan.ai’s SaaS tool which enables users to record/upload their videos and then create personalized copies of such a video and deliver them to their customers. \n Worked with the MediaRecorder and MediaStream API to allow video recording on the platform.\n Revamped the existing workflows with a new theme and UX.\n Wrote base components over MUI (Styling library).Stack - React, TypeScript, Redux, MUI, Tailwind",
-
+          "# Myna - Gan Studio's SaaS platform. \n ## ☉ Myna is Gan.ai’s SaaS tool which enables users to record/upload their videos and then create personalized copies of such a video and deliver them to their customers. \n ## ☉ Worked with the MediaRecorder and MediaStream API to allow video recording on the platform. \n ## ☉ Revamped the existing workflows with a new theme and UX. \n ## ☉ Wrote base components over MUI (Styling library).Stack - React, TypeScript, Redux, MUI, Tailwind",
         tech_used: [
           { title: "", icon: "/assets/tech/react1.png" },
           { title: "", icon: "/assets/tech/redux.png" },
@@ -34,7 +33,7 @@ const work_data: WorkData[] = [
       {
         project_title: "Internal tools",
         description:
-          "asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd ",
+          "Participated in development of an internal tool to ease the pipeline for video generation.",
         tech_used: [
           { title: "", icon: "/assets/tech/react1.png" },
           { title: "", icon: "/assets/tech/redux.png" },
@@ -52,9 +51,9 @@ const work_data: WorkData[] = [
       "at Tekion. Time well spent with a large-scale React SPA, a consumer-facing platform and learning the basics of JS ecosystem.",
     projects: [
       {
-        project_title: "DMS - F&I",
+        project_title: "Dealer Management System - F&I",
         description:
-          "asdasd asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd ",
+          "# Was part of the F&I Team at Tekion. \n ## ☉ Exposed to workings of a very large Single Page Application, antd library, pusher notfications. \n ☉ Wrote a scalable system to support an increasing number of wrapped third party APIs for external information providers.",
         tech_used: [
           { title: "", icon: "/assets/tech/react1.png" },
           { title: "", icon: "/assets/tech/redux.png" },
@@ -64,9 +63,9 @@ const work_data: WorkData[] = [
         ],
       },
       {
-        project_title: "Consumer Repo",
+        project_title: "Digital Retail System",
         description:
-          "asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd asdasdasdasdasdasd ",
+          "## ☉ Worked as a UI developer with a team responsible for developing Tekion's consumer-facing DRS from scratch which is a platform for shopping cars online and having a zero-contact sales experience.\n ## ☉ Participated in wricng base components to be used applicacon wide by other members of the team. \n ## ☉ DRS is meant to be scalable for various OEMs as they have their own designs and flows, and has abilices to be customizable by a dealership. Worked on integrating customization abilities with the Dealership Management System.",
         tech_used: [
           { title: "", icon: "/assets/tech/next3.png" },
           { title: "", icon: "/assets/tech/redux.png" },
--- components/shared/themeSwitch.tsx
+++ components/shared/themeSwitch.tsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 import { Moon } from "lucide-react";
-
+import { motion } from "framer-motion";
 const modes = ["system", "light", "dark"];
 
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, resolvedTheme } = useTheme();
   const [index, setCurrentIndex] = useState(0);
 
   // useEffect only runs on the client, so now we can safely show the UI
@@ -17,23 +17,28 @@ const ThemeSwitch = () => {
   useEffect(() => {
     setTheme(modes[index]);
   }, [index]);
+
   if (!mounted) {
     return null;
   }
 
   return (
-    <button
-      className="mr-3 rounded-2xl bg-transparent px-4 py-1 text-base text-slate-400"
+    <motion.button
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="mr-3 rounded-full border border-gray-300 bg-transparent bg-white px-5 py-2 text-sm text-gray-600 shadow-md  transition-colors dark:bg-black  dark:text-white"
       onClick={() => setCurrentIndex((prev) => (prev + 1) % modes.length)}
     >
       <span className="flex items-center gap-2">
+        {theme} mode
         <Moon
           strokeWidth="0.02rem"
-          fill={theme === "system" || theme === "dark" ? "#222" : "#fff"}
+          size={20}
+          fill={resolvedTheme === "dark" ? "#222" : "#fff"}
         />
-        {theme} mode
       </span>
-    </button>
+    </motion.button>
   );
 };
 
