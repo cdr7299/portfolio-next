0be8ebc4b39f8b89898e3c58bf52feaa544f6822
Vineet Sawhney <gadgetgeek95@gmail.com>
2023-09-10 10:51:05 -0400

    Fix nav in mobile (#28)

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -2,26 +2,27 @@
 
 import Link from "next/link";
 import useScroll from "@/lib/hooks/use-scroll";
-import { useSignInModal } from "./sign-in-modal";
 import { Session } from "next-auth";
 import ThemeSwitch from "../shared/themeSwitch";
 
 export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
 
   return (
     <>
       {/* <SignInModal /> */}
       <div
-        className={`fixed top-0 hidden w-full justify-center md:flex ${
+        className={`fixed top-0 flex w-full justify-center ${
           scrolled
             ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl dark:border-gray-700 dark:bg-[#0d1117]/50 dark:backdrop-blur-xl"
             : "bg-white/0"
         } z-30 transition-all`}
       >
         <div className="mx-5 flex h-16 w-full items-center justify-between">
-          <Link href="/" className="flex items-center font-display text-2xl">
+          <Link
+            href="/"
+            className="flex items-center font-display text-lg md:text-2xl "
+          >
             <p>Hello &#128075;</p>
           </Link>
           <div className="flex items-center">
