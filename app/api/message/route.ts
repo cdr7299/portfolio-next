e08c21a33f4e2fb0533494a72d16eb3b1f4a4b22
Vineet Sawhney <gadgetgeek95@gmail.com>
2024-03-25 18:21:27 -0400

    remove unused components

--- app/api/message/route.ts
+++ app/api/message/route.ts
@@ -1,43 +1,42 @@
-import { NextResponse } from "next/server";
-import prisma from "../../../lib/prisma";
+// import prisma from "../../../lib/prisma";
 
 export async function POST(req: Request) {
   const body = await req.json();
-  try {
-    const result: any = await prisma.message.create({
-      data: {
-        name: body.name,
-        email: body.email,
-        message: body.message,
-        phone: body.phone,
-      },
-    });
-    return NextResponse.json(result);
-  } catch (err: any) {
-    console.log("Error Code", err.code, err.message);
-    switch (err.code) {
-      case "P2002":
-        return new NextResponse(
-          JSON.stringify({
-            error: "Duplicate Key",
-            statusCode: err.code,
-            message: err.message,
-          }),
-          {
-            status: 500,
-          },
-        );
-      default:
-        return new NextResponse(
-          JSON.stringify({
-            error: "Missing field attributes",
-            statusCode: err.code,
-            message: err.message,
-          }),
-          {
-            status: 500,
-          },
-        );
-    }
-  }
+  // try {
+  //   const result: any = await prisma.message.create({
+  //     data: {
+  //       name: body.name,
+  //       email: body.email,
+  //       message: body.message,
+  //       phone: body.phone,
+  //     },
+  //   });
+  //   return NextResponse.json(result);
+  // } catch (err: any) {
+  //   console.log("Error Code", err.code, err.message);
+  //   switch (err.code) {
+  //     case "P2002":
+  //       return new NextResponse(
+  //         JSON.stringify({
+  //           error: "Duplicate Key",
+  //           statusCode: err.code,
+  //           message: err.message,
+  //         }),
+  //         {
+  //           status: 500,
+  //         },
+  //       );
+  //     default:
+  //       return new NextResponse(
+  //         JSON.stringify({
+  //           error: "Missing field attributes",
+  //           statusCode: err.code,
+  //           message: err.message,
+  //         }),
+  //         {
+  //           status: 500,
+  //         },
+  //       );
+  //   }
+  // }
 }
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -8,27 +8,24 @@ export default function NavBar() {
   const scrolled = useScroll(50);
 
   return (
-    <>
-      {/* <SignInModal /> */}
-      <div
-        className={`fixed top-0 flex w-full justify-center ${
-          scrolled
-            ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl dark:border-gray-700 dark:bg-[#0d1117]/50 dark:backdrop-blur-xl"
-            : "bg-white/0"
-        } z-30 transition-all`}
-      >
-        <div className="mx-2 flex h-16 w-full items-center justify-between md:mx-5">
-          <Link
-            href="/"
-            className="flex items-center font-display text-lg md:text-2xl "
-          >
-            <p>Hello &#128075;</p>
-          </Link>
-          <div className="flex items-center">
-            <ThemeSwitch />
-          </div>
+    <div
+      className={`fixed top-0 flex w-full justify-center ${
+        scrolled
+          ? "border-b border-gray-200 bg-white/50 backdrop-blur-xl dark:border-gray-700 dark:bg-[#0d1117]/50 dark:backdrop-blur-xl"
+          : "bg-white/0"
+      } z-30 transition-all`}
+    >
+      <div className="mx-2 flex h-16 w-full items-center justify-between md:mx-5">
+        <Link
+          href="/"
+          className="flex items-center font-display text-lg md:text-2xl "
+        >
+          <p>Hello &#128075;</p>
+        </Link>
+        <div className="flex items-center">
+          <ThemeSwitch />
         </div>
       </div>
-    </>
+    </div>
   );
 }
